f34bc6175d66ca0504e25cecf97ef4fc946d680a
ViNx3h <vinhphuochongnnn@gmail.com>
2024-09-10 16:58:42 +0700

    ..

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,5 +1,4 @@
 import { defineBackend } from '@aws-amplify/backend';
-import { downloadData } from 'aws-amplify/storage';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { storage } from './storage/resource';
@@ -9,19 +8,3 @@ defineBackend({
   data,
   storage,
 });
-
-
-try {
-  const result = downloadData({
-    path: 'album/2024/1.jpg',
-    options: {
-      // Alternatively, provide bucket name from console and associated region
-      bucket: {
-        bucketName: 'amplify-d2x7let61n8fca-ma-amplifyteamdrivebucket28-90epohn8if9i',
-        region: 'ap-southeast-1'
-      }
-    }
-  }).result;
-} catch (error) {
-  console.log(`Error: ${error}`);
-}
